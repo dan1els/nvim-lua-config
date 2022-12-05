92e6ac8c58853561413c3aa0ca830bfc1ed81ca0
Evgenii Riabov <eryabov@inpromconsult.ru>
2022-12-05 14:44:42 +0300

    status line

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -41,5 +41,9 @@ return require('packer').startup(function(use)
     run = ':TSUpdate'
   }
 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
 
