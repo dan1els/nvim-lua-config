77656b0c7cd843919fd57ba71a1b086f68ce26b8
Evgenii Riabov <eryabov@inpromconsult.ru>
2022-12-06 16:58:02 +0300

    Added YCM

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nvim-lua-config
+
+My personal config for neovim.
+
+Additional actions:
+
+1. compile [YCM](https://github.com/tabnine/YouCompleteMe) -- files are located in ~/.local/share/nvim/site/pack/packer/start/YouCompleteMe
--- lua/init.lua
+++ lua/init.lua
@@ -4,3 +4,6 @@ require('hotkeys')
 
 -- auto changedir 
 vim.opt.autochdir = true
+
+
+vim.fn.setenv("JAVA_TOOL_OPTIONS", "-javaagent:/Users/evgenii/.m2/repository/org/projectlombok/lombok-1.18.24.jar -Xbootclasspath/p:/Users/evgenii/.m2/repository/org/projectlombok/lombok-1.18.24.jar")
