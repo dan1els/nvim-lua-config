e48d339fcdcd1d9a54d9e8c120853c72e8988655
Evgenii Riabov <eryabov@inpromconsult.ru>
2023-03-21 13:00:25 +0300

    debugmode reuseable after fix

--- lua/debugmode.lua
+++ lua/debugmode.lua
@@ -63,35 +63,35 @@ end
 
 -- require'telescope'.extensions.dap.configurations{}
 
-local layerFactory = function() 
-  local debugmode = libmodal.layer.new({})
-  debugmode:map("n", "uu", function() dapui.toggle() end, {})
-  debugmode:map("n", "+", switch, {})
-  debugmode:map("n", "-", switch, {})
-  debugmode:map("n", "e", function() dapui.eval()  end, {})
-  debugmode:map("n", "R", function() dap.continue()  end, {})
-  debugmode:map("n", "b", function() dap.toggle_breakpoint()  end, {})
-  debugmode:map("n", "n", function() dap.step_over()  end, {})
-  debugmode:map("n", "N", function() dap.step_into()  end, {})
-  debugmode:map("n", "k", function() dap.up()  end, {})
-  debugmode:map("n", "j", function() dap.down()  end, {})
-  debugmode:map("n", "C", function() require'telescope'.extensions.dap.commands {} end, {})
-  debugmode:map("n", "B", function() require'telescope'.extensions.dap.list_breakpoints{} end, {})
-  debugmode:map("n", "V", function() require'telescope'.extensions.dap.variables{} end, {})
-  debugmode:map("n", "F", function() require'telescope'.extensions.dap.frames{} end, {})
-  debugmode:map("n", "<esc>", function()  vim.g.libmodalActiveLayerName = nil debugmode:exit() end, {})
-  vim.g.libmodalActiveLayerName = 'DEBUG'
-  return debugmode
-end;
-vim.keymap.set("n", "<leader>d", function() layerFactory():enter() end, {})
+local debugmode = libmodal.layer.new({})
+debugmode:map("n", "uu", function() dapui.toggle() end, {})
+debugmode:map("n", "+", switch, {})
+debugmode:map("n", "-", switch, {})
+debugmode:map("n", "e", function() dapui.eval()  end, {})
+debugmode:map("n", "R", function() dap.continue()  end, {})
+debugmode:map("n", "b", function() dap.toggle_breakpoint()  end, {})
+debugmode:map("n", "n", function() dap.step_over()  end, {})
+debugmode:map("n", "N", function() dap.step_into()  end, {})
+debugmode:map("n", "k", function() dap.up()  end, {})
+debugmode:map("n", "j", function() dap.down()  end, {})
+debugmode:map("n", "C", function() require'telescope'.extensions.dap.commands {} end, {})
+debugmode:map("n", "B", function() require'telescope'.extensions.dap.list_breakpoints{} end, {})
+debugmode:map("n", "V", function() require'telescope'.extensions.dap.variables{} end, {})
+debugmode:map("n", "F", function() require'telescope'.extensions.dap.frames{} end, {})
+debugmode:map("n", "<esc>", function()  vim.g.libmodalActiveLayerName = nil debugmode:exit() end, {})
+
+vim.keymap.set("n", "<leader>d", function() debugmode:enter() vim.g.libmodalActiveLayerName = 'DEBUG' end, {})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  layerFactory():enter()
+  debugmode:enter()
+  vim.g.libmodalActiveLayerName = 'DEBUG'
   dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
+  debugmode:exit()
   dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
+  debugmode:exit()
   dapui.close()
 end
